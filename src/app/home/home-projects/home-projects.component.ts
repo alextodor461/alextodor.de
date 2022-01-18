2d3193f82ba18cfeec765b3eca6e564cab6c2c49
asslikealex <alex_todor@yahoo.com>
2022-01-18 20:31:52 +0100

    adjusdments

--- src/app/home/home-projects/home-projects.component.ts
+++ src/app/home/home-projects/home-projects.component.ts
@@ -11,20 +11,44 @@ export class HomeProjectsComponent implements OnInit {
     (<HTMLInputElement>document.getElementById("projects-main-div")).innerHTML = '';
     (<HTMLInputElement>document.getElementById("projects-main-div")).innerHTML = 
     `<div class="projects">
-      <div class="project-div">
+      <div  class="project-div" (mouseover)="onMouseOver()">
         <img src="assets/img/code.jpg">
+
+        <div id="overlay" class="overlay, d-none">
+          <h3>EL POLLO LOCO</h3>
+          <p>JavaScript - based jump and run game.</p>
+          <a href="">Try out</a>
+        </div>
       </div>
-  
+ 
       <div class="project-div">
         <img src="assets/img/code2.jpg">
+
+        <div class="overlay, d-none">
+          <h3>EL POLLO LOCO</h3>
+          <p>JavaScript - based jump and run game.</p>
+          <a href="">Try out</a>
+        </div>
       </div>
 
       <div class="project-div">
         <img src="assets/img/code3.jpg">
+
+        <div class="overlay, d-none">
+          <h3>EL POLLO LOCO</h3>
+          <p>JavaScript - based jump and run game.</p>
+          <a href="">Try out</a>
+        </div>
       </div>
 
       <div class="project-div">
         <img src="assets/img/code4.jpg">
+
+        <div class="overlay, d-none">
+          <h3>EL POLLO LOCO</h3>
+          <p>JavaScript - based jump and run game.</p>
+          <a href="">Try out</a>
+        </div>
       </div>
     </div>`;
    }  
@@ -35,10 +59,22 @@ export class HomeProjectsComponent implements OnInit {
     `<div class="projects">
       <div class="project-div">
         <img src="assets/img/code4.jpg">
+
+        <div class="overlay">
+          <h3>EL POLLO LOCO</h3>
+          <p>Angular - based jump and run game.</p>
+          <a target="_blanc" href="http://alex-todor.developerakademie.net/El%20Pollo%20Loco">Try out</a>
+        </div>
       </div>
  
       <div class="project-div">
         <img src="assets/img/code4.jpg">
+
+        <div class="overlay">
+          <h3>EL POLLO LOCO</h3>
+          <p>Angular - based jump and run game.</p>
+          <a href="">Try out</a>
+        </div>
       </div>
     </div>`;
    }
@@ -51,6 +87,11 @@ export class HomeProjectsComponent implements OnInit {
         <img src="assets/img/css.png">
       </div>
      </div>`;
+   }
+
+   onMouseOver(){
+    (<HTMLInputElement>document.getElementById("overlay")).classList.remove('d-none');
+    console.log('MOUUSEEE PVVVERR');
    }
 
   ngOnInit(){
