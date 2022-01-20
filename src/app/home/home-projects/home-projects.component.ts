116b4ef834591406ca05d1b566272f42e66a1ee6
asslikealex <alex_todor@yahoo.com>
2022-01-20 17:42:01 +0100

    responsive for 800 and 600px

--- src/app/home/home-projects/home-projects.component.ts
+++ src/app/home/home-projects/home-projects.component.ts
@@ -1,4 +1,6 @@
+import { HtmlAstPath } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { HomeSkillsComponent } from '../home-skills/home-skills.component';
 
 @Component({ 
   selector: 'app-home-projects',
@@ -6,97 +8,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-projects.component.scss']
 })
 export class HomeProjectsComponent implements OnInit {
+
+  allProjects = [
+    {
+      name: "El Pollo Locco",
+      url: "http://alex-todor.developerakademie.net/El%20Pollo%20Loco",
+      text: "based jump and run game.",
+      img: "assets/img/code3.jpg",
+      category: "javascript"
+    },
+
+    {
+      name: "KanbanBoard",
+      url: "https://mygame.com",
+      img: "assets/img/code2.jpg",
+      category: "javascript"
+    },
+
+    {
+      name: "El Pollo Locco",
+      url: "https://mygame.com",
+      img: "assets/img/code2.jpg",
+      category: "angular"
+    },
+
+    {
+      name: "El Pollo Locco",
+      url: "https://mygame.com",
+      img: "assets/img/code2.jpg",
+      category: "angular"
+    }
+  ];
+
+  filteredProjects = this.allProjects;
+
+  showCategory(category: any){
+      if(category == 'all'){
+        this.filteredProjects = this.allProjects;
+      }else{
+        this.filteredProjects = this.allProjects.filter(p => p.category === category);
+      }
+  }
+
+  showOverlay(){
+    (<HTMLInputElement>document.getElementById('overlay')).classList.remove('d-none'); 
+  }
   
-  showAllProjects(){
-    (<HTMLInputElement>document.getElementById("projects-main-div")).innerHTML = '';
-    (<HTMLInputElement>document.getElementById("projects-main-div")).innerHTML = 
-    `<div class="projects">
-      <div  class="project-div" (mouseover)="onMouseOver()">
-        <img src="assets/img/code.jpg">
-
-        <div id="overlay" class="overlay, d-none">
-          <h3>EL POLLO LOCO</h3>
-          <p>JavaScript - based jump and run game.</p>
-          <a href="">Try out</a>
-        </div>
-      </div>
- 
-      <div class="project-div">
-        <img src="assets/img/code2.jpg">
-
-        <div class="overlay, d-none">
-          <h3>EL POLLO LOCO</h3>
-          <p>JavaScript - based jump and run game.</p>
-          <a href="">Try out</a>
-        </div>
-      </div>
-
-      <div class="project-div">
-        <img src="assets/img/code3.jpg">
-
-        <div class="overlay, d-none">
-          <h3>EL POLLO LOCO</h3>
-          <p>JavaScript - based jump and run game.</p>
-          <a href="">Try out</a>
-        </div>
-      </div>
-
-      <div class="project-div">
-        <img src="assets/img/code4.jpg">
-
-        <div class="overlay, d-none">
-          <h3>EL POLLO LOCO</h3>
-          <p>JavaScript - based jump and run game.</p>
-          <a href="">Try out</a>
-        </div>
-      </div>
-    </div>`;
-   }  
-
-   showAngularProjects(){
-    (<HTMLInputElement>document.getElementById("projects-main-div")).innerHTML = '';
-    (<HTMLInputElement>document.getElementById("projects-main-div")).innerHTML = 
-    `<div class="projects">
-      <div class="project-div">
-        <img src="assets/img/code4.jpg">
-
-        <div class="overlay">
-          <h3>EL POLLO LOCO</h3>
-          <p>Angular - based jump and run game.</p>
-          <a target="_blanc" href="http://alex-todor.developerakademie.net/El%20Pollo%20Loco">Try out</a>
-        </div>
-      </div>
- 
-      <div class="project-div">
-        <img src="assets/img/code4.jpg">
-
-        <div class="overlay">
-          <h3>EL POLLO LOCO</h3>
-          <p>Angular - based jump and run game.</p>
-          <a href="">Try out</a>
-        </div>
-      </div>
-    </div>`;
-   }
-
-   showJavascriptProjects(){
-    (<HTMLInputElement>document.getElementById("projects-main-div")).innerHTML = '';
-    (<HTMLInputElement>document.getElementById("projects-main-div")).innerHTML = 
-    `<div class="projects">
-      <div class="project-div">
-        <img src="assets/img/css.png">
-      </div>
-     </div>`;
-   }
-
-   onMouseOver(){
-    (<HTMLInputElement>document.getElementById("overlay")).classList.remove('d-none');
-    console.log('MOUUSEEE PVVVERR');
-   }
-
   ngOnInit(){
-    this.showAngularProjects(); 
+    /*this.showAngularProjects(); 
     this.showJavascriptProjects();
-    this.showAllProjects();
+    this.showAllProjects();*/
   }
 }
