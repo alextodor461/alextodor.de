a48ae42987859056beb7433aa6d116f8814e45a0
asslikealex <alex_todor@yahoo.com>
2022-04-26 10:04:07 +0200

    Version 2.0

--- src/app/home/home-main/home-main.component.ts
+++ src/app/home/home-main/home-main.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-main',
   templateUrl: './home-main.component.html',
-  styleUrls: ['./home-main.component.scss']
+  styleUrls: ['./home-main.component.scss'],
 })
 
 export class HomeMainComponent implements OnInit {
